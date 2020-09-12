3786a85db62bcc5b50dad351b8da5c1ded607ea8
Barrows <clayton.barrows@nrel.gov>
2020-09-11 21:53:59 -0600

    updating hydro demo

--- script/PowerSimulations_examples/5_hydropower_simulation.jl
+++ script/PowerSimulations_examples/5_hydropower_simulation.jl
@@ -42,7 +42,7 @@ PSI.JuMP._wrap_in_math_mode(str) = "\$\$ $(replace(str, "__"=>"")) \$\$"
 # using the `DeviceModel`. We can also adjust how simulations are configured to represent
 # different decision making processes and the information flow between those processes.
 
-# ### Hydropower `DeviceModel`s
+# ## Hydropower `DeviceModel`s
 
 # First, the assignment of device formulations to particular device types gives us control
 # over the representation of devices. This is accomplished by defining `DeviceModel`
@@ -56,65 +56,131 @@ PSI.JuMP._wrap_in_math_mode(str) = "\$\$ $(replace(str, "__"=>"")) \$\$"
 
 #nb TypeTree(PSI.AbstractHydroFormulation, scopesep = "\n", init_expand = 5)
 
+# ### `FixedOutput`
+
 # Let's see what some of the different combinations create. First, let's apply the
-# `HydroDispatchRunOfRiver` formulation to the `HydroEnergyReservoir` generators, and the
-# `FixedOutput` formulation to `HydroDispatch` generators.
+# `FixedOutput` formulation to the `HydroEnergyReservoir` generators.
 #  - The `FixedOutput` formulation just acts
 # like a load subtractor, forcing the system to accept it's generation.
-#  - The `HydroDispatchRunOfRiver` formulation represents the the energy flowing out of
-# a reservoir. The model can choose to produce power with that energy or just let it spill by.
 
-devices = Dict{Symbol,DeviceModel}(
-    :Hyd1 => DeviceModel(HydroEnergyReservoir, HydroDispatchRunOfRiver),
-    :Hyd2 => DeviceModel(HydroDispatch, FixedOutput),
-    :Load => DeviceModel(PowerLoad, StaticPowerLoad),
-);
+devices = Dict{Symbol,DeviceModel}(:Hyd1 => DeviceModel(HydroDispatch, FixedOutput));
 
 template = PSI.OperationsProblemTemplate(CopperPlatePowerModel, devices, Dict(), Dict());
 
-op_problem = PSI.OperationsProblem(GenericOpProblem, template, c_sys5_hy_uc, horizon = 2)
+op_problem = PSI.OperationsProblem(GenericOpProblem, template, demo_sys, horizon = 2)
 
 # Now we can see the resulting JuMP model:
 
 op_problem.psi_container.JuMPmodel
 
-# The first two constraints are the power balance constraints that require the generation
-# from the controllable `HydroEnergyReservoir` generators to be equal to the load (flat 10.0 for all time periods)
-# minus the generation from the `HydroDispatch` generators [1.97, 1.983, ...]. The 3rd and 4th
-# constraints limit the output of the `HydroEnergyReservoir` generator to the limit defined by the
-# `max_activepwoer` forecast. And the last 4 constraints are the lower and upper bounds of
-# the `HydroEnergyReservoir` operating range.
+# The two constrants are the power balance constarints in each of the two periods. Since
+# we haven't added a load or any coontrollable generation to this model, it is infeasible.
+# But you can see that the values correspond to the `get_max_active_power` forecast for the
+# `HydroDispatch2` generator.
 
-#-
+get_forecast_values(
+    Deterministic,
+    get_component(HydroDispatch, demo_sys, "HydroDispatch2"),
+    DateTime(today()),
+    "get_max_active_power",
+    2,
+)
 
-# Next, let's apply the `HydroDispatchReservoirFlow` formulation to the `HydroEnergyReservoir` generators.
+# ### `HydroDispatchRunOfRiver`
+
+# Next, let's apply the `HydroDispatchRunOfRiver` formulation to the `HydroEnergyReservoir`
+# generators.
+#  - The `HydroDispatchRunOfRiver` formulation represents the the energy flowing out of
+# a reservoir. The model can choose to produce power with that energy or just let it spill by.
+
 devices = Dict{Symbol,DeviceModel}(
-    :Hyd1 => DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirFlow),
-    :Load => DeviceModel(PowerLoad, StaticPowerLoad),
+    :Hyd1 => DeviceModel(HydroEnergyReservoir, HydroDispatchRunOfRiver),
 );
 
 template = PSI.OperationsProblemTemplate(CopperPlatePowerModel, devices, Dict(), Dict());
 
-op_problem = PSI.OperationsProblem(GenericOpProblem, template, c_sys5_hy_uc, horizon = 2)
+op_problem = PSI.OperationsProblem(GenericOpProblem, template, demo_sys, horizon = 2)
+
+op_problem.psi_container.JuMPmodel
+
+# The first two constraints are the power balance constraints that require the generation
+# from the controllable `HydroEnergyReservoir` generators to be equal to the (non-existant)
+# load. The 3rd through 6th constraints limit the output of the `HydroEnergyReservoir`
+# generators to the limit defined by the `get_max_active_pwoer` forecast.
+# And the remainider of the constraints are the lower and upper bounds of
+# the `HydroEnergyReservoir` operating ranges.
+
+# ### `HydroDispatchReservoirStorage`
+
+# Next, let's apply the `HydroDispatchReservoirStorage` formulation to the `HydroEnergyReservoir` generators.
+devices = Dict{Symbol,DeviceModel}(
+    :Hyd1 => DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirStorage),
+);
+
+template = PSI.OperationsProblemTemplate(CopperPlatePowerModel, devices, Dict(), Dict());
+
+op_problem = PSI.OperationsProblem(GenericOpProblem, template, demo_sys, horizon = 2)
 
 # And, the resulting JuMP model:
 
 op_problem.psi_container.JuMPmodel
 
-# Finally, let's apply the `HydroDispatchReservoirStorage` formulation to the `HydroEnergyReservoir` generators.
+# This time, the first and third constraints represent the energy balance in each `HydroEnergyReservoir`
+# reservoirs in the first time period, accounting for  the energy released through spillage
+# `Sp` and power production `P`, and the inflow energy and the initial stored energy are rerpresented
+# on the right hand side from the corresponding `get_inflow` and `get_storage_capacity` forecasts, reespectively.
+
+get_forecast_values(
+    Deterministic,
+    get_component(HydroEnergyReservoir, demo_sys, "HydroDispatch3"),
+    DateTime(today()),
+    "get_storage_capacity",
+    2,
+)
+
+get_forecast_values(
+    Deterministic,
+    get_component(HydroEnergyReservoir, demo_sys, "HydroDispatch3"),
+    DateTime(today()),
+    "get_inflow",
+    2,
+)
+
+# The second and fourth constraints represent the energy balance in the reservoirs in subsequent
+# periods. Constraints 5 and 6 are the power balance constraints, followed by lower bounds of
+# zero for power (`P`), energy (`E`), and spillage (`Sp`) variables, and upper boounds for
+# `P` and `E` defined by the `active_power_limits.max` and `storage_capacity` parameters.
+
+# ### `HydroDispatchReservoirBudget`
+
+# Finally, let's apply the `HydroDispatchReservoirBudget` formulation to the `HydroEnergyReservoir` generators.
 
 devices = Dict{Symbol,DeviceModel}(
-    :Hyd1 => DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirStorage),
-    :Load => DeviceModel(PowerLoad, StaticPowerLoad),
+    :Hyd1 => DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirBudget),
 );
 
 template = PSI.OperationsProblemTemplate(CopperPlatePowerModel, devices, Dict(), Dict());
 
-op_problem = PSI.OperationsProblem(GenericOpProblem, template, c_sys5_hy_uc, horizon = 2)
+op_problem = PSI.OperationsProblem(GenericOpProblem, template, demo_sys, horizon = 2)
 
 #-
 
 op_problem.psi_container.JuMPmodel
+
+# Again, the first and second constraints represent the energy balance. The third and fourth
+# constraints represent the energy budget for each `HydroEnergyReservoir` generator over the
+# optimization horizon. The right hand side of these constraints correspond to the sums of
+# the values of the "get_hydro_budget" forecast multiplied by the `storage_capacity`:
+g = get_component(HydroEnergyReservoir, demo_sys, "HydroDispatch3")
+get_forecast(
+    Deterministic,
+    g,
+    DateTime(today()),
+    "get_hydro_budget",
+    2,
+).data .* get_storage_capacity(g) |> sum
+
+# The remainder of the constraints correspond to the lower and upper bounds of each generator in each time period.
 
 # ### Multi-Stage `SimulationSequence`
 # The purpose of a multi-stage simulation is to represent scheduling decisions consistently
@@ -126,10 +192,10 @@ op_problem.psi_container.JuMPmodel
 # requirements/capabilities.
 
 devices = Dict(
-    :Generators => DeviceModel(ThermalStandard, ThermalDispatch),
+    :Generators => DeviceModel(ThermalStandard, ThermalBasicUnitCommitment),
     :Loads => DeviceModel(PowerLoad, StaticPowerLoad),
     :HydroEnergyReservoir =>
-        DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirStorage),
+        DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirBudget),
 )
 template_md = OperationsProblemTemplate(CopperPlatePowerModel, devices, Dict(), Dict());
 
@@ -137,12 +203,14 @@ template_md = OperationsProblemTemplate(CopperPlatePowerModel, devices, Dict(), 
 # problems.
 
 devices = Dict(
-    :Generators => DeviceModel(ThermalStandard, ThermalDispatch),
+    :Generators => DeviceModel(ThermalStandard, ThermalBasicUnitCommitment),
     :Loads => DeviceModel(PowerLoad, StaticPowerLoad),
     :HydroEnergyReservoir =>
-        DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirStorage),
+        DeviceModel(HydroEnergyReservoir, HydroDispatchReservoirBudget),
 )
 template_da = OperationsProblemTemplate(CopperPlatePowerModel, devices, Dict(), Dict());
+
+op_problem = PSI.OperationsProblem(GenericOpProblem, template_md, demo_wk_sys, horizon = 2)
 
 #-
 
@@ -150,14 +218,14 @@ stages_definition = Dict(
     "MD" => Stage(
         GenericOpProblem,
         template_md,
-        c_sys5_hy_wk,
+        demo_wk_sys,
         solver,
         system_to_file = false,
     ),
     "DA" => Stage(
         GenericOpProblem,
         template_da,
-        c_sys5_hy_uc,
+        demo_da_sys,
         solver,
         system_to_file = false,
     ),
@@ -179,8 +247,8 @@ sequence = SimulationSequence(
             affected_variables = [PSI.ACTIVE_POWER],
         ),
     ),
-    cache = Dict(("MD", "DA") => StoredEnergy(HydroEnergyReservoir, PSI.ENERGY)),
-    ini_cond_chronology = IntraStageChronology(),
+    #cache = Dict(("MD", "DA") => StoredEnergy(HydroEnergyReservoir, PSI.ENERGY)),
+    ini_cond_chronology = InterStageChronology(),
 );
 
 #-
@@ -217,39 +285,39 @@ stages_definition = Dict(
     "MD" => Stage(
         GenericOpProblem,
         template_md,
-        c_sys5_hy_wk,
+        demo_wk_sys,
         solver,
         system_to_file = false,
     ),
     "DA" => Stage(
         GenericOpProblem,
         template_da,
-        c_sys5_hy_uc,
+        demo_da_sys,
         solver,
         system_to_file = false,
     ),
     "ED" => Stage(
         GenericOpProblem,
         template_da,
-        c_sys5_hy_ed,
+        demo_rt_sys,
         solver,
         system_to_file = false,
     ),
 )
 
 sequence = SimulationSequence(
-    step_resolution = Hour(48),
+    step_resolution = Hour(72),
     order = Dict(1 => "MD", 2 => "DA", 3 => "ED"),
     feedforward_chronologies = Dict(
-        ("MD" => "DA") => Synchronize(periods = 2),
+        ("MD" => "DA") => Synchronize(periods = 1),
         ("DA" => "ED") => Synchronize(periods = 24),
     ),
     intervals = Dict(
-        "MD" => (Hour(48), Consecutive()),
+        "MD" => (Hour(72), Consecutive()),
         "DA" => (Hour(24), Consecutive()),
         "ED" => (Hour(1), Consecutive()),
     ),
-    horizons = Dict("MD" => 2, "DA" => 24, "ED" => 12),
+    horizons = Dict("MD" => 3, "DA" => 24, "ED" => 12),
     feedforward = Dict(
         ("DA", :devices, :HydroEnergyReservoir) => IntegralLimitFF(
             variable_source_stage = PSI.ACTIVE_POWER,
@@ -260,8 +328,8 @@ sequence = SimulationSequence(
             affected_variables = [PSI.ACTIVE_POWER],
         ),
     ),
-    cache = Dict(("MD", "DA") => StoredEnergy(HydroEnergyReservoir, PSI.ENERGY)),
-    ini_cond_chronology = IntraStageChronology(),
+    #cache = Dict(("MD", "DA") => StoredEnergy(HydroEnergyReservoir, PSI.ENERGY)),
+    ini_cond_chronology = InterStageChronology(),
 );
 
 #-
--- script/PowerSimulations_examples/make_hydro_data.jl
+++ script/PowerSimulations_examples/make_hydro_data.jl
@@ -14,18 +14,18 @@ rawsys = PowerSystems.PowerSystemTableData(
     joinpath(data_dir, "5-bus-hydro", "user_descriptors.yaml");
     generator_mapping_file = joinpath(data_dir, "5-bus-hydro", "generator_mapping.yaml"),
 )
-c_sys5_hy_uc = System(
+
+demo_sys = System(
     rawsys,
     timeseries_metadata_file = joinpath(
         data_dir,
-        "forecasts",
-        "5bus_ts",
-        "7day",
-        "timeseries_pointers_da_7day.json",
+        "5-bus-hydro",
+        "timeseries_pointers_da.json",
     ),
     time_series_in_memory = true,
 )
-c_sys5_hy_ed = System(
+
+demo_rt_sys = System(
     rawsys,
     timeseries_metadata_file = joinpath(
         data_dir,
@@ -36,75 +36,51 @@ c_sys5_hy_ed = System(
     ),
     time_series_in_memory = true,
 )
-c_sys5_hy_wk = System(rawsys, time_series_in_memory = true)
 
+demo_da_sys =  System(
+    rawsys,
+    timeseries_metadata_file = joinpath(
+        data_dir,
+        "forecasts",
+        "5bus_ts",
+        "7day",
+        "timeseries_pointers_da_7day.json"),
+    time_series_in_memory = true,
+    )
 
-n = 3# number of days
-
+demo_wk_sys =  System(
+    rawsys,
+    timeseries_metadata_file = joinpath(
+        data_dir,
+        "forecasts",
+        "5bus_ts",
+        "7day",
+        "timeseries_pointers_wk_7day.json"),
+    time_series_in_memory = true,
+    )
+### helper to convert ts to daily resolution
+#=
+n = 7# number of days
 # And an hourly system with longer time scales
 MultiDay = collect(
-    get_forecast_initial_times(c_sys5_hy_uc)[1]:Hour(24):(get_forecast_initial_times(
-        c_sys5_hy_uc,
-    )[1]+Day(n)),
+    get_forecast_initial_times(demo_da_sys)[1]:Hour(24):(get_forecast_initial_times(
+        demo_da_sys,
+    )[1]+Day(n-1)),
 );
 
+fname = joinpath(data_dir,"forecasts","5bus_ts","7day","wk","wk_wind.csv")
+df = CSV.read(fname, DataFrame)
+ta = []
+for col in names(df)
+    if col != "DateTime"
+        fc = TimeArray(df.DateTime, df[:,col])
+        fc_values = [mean(values(fc[Date.(timestamp(fc)).==t])) for t in MultiDay]
 
-for load in get_components(PowerLoad, c_sys5_hy_uc)
-    fc = get_forecast_values(
-        Deterministic,
-        load,
-        get_forecast_initial_times(c_sys5_hy_uc)[1],
-        "get_max_active_power",
-    )
-    fc_values = [mean(values(fc[Date.(timestamp(fc)).==t])) for t in MultiDay]
-
-    wk_fc = TimeArray(MultiDay, fc_values)
-
-    add_forecast!(
-        c_sys5_hy_wk,
-        get_component(PowerLoad, c_sys5_hy_wk, get_name(load)),
-        Deterministic("get_max_active_power", wk_fc),
-    )
-end
-
-for gen in get_components(HydroGen, c_sys5_hy_uc)
-    fc = get_forecast_values(
-        Deterministic,
-        gen,
-        get_forecast_initial_times(c_sys5_hy_uc)[1],
-        "get_max_active_power",
-    )
-    fc_values = []
-    for (ix, t) in enumerate(MultiDay)
-        push!(fc_values, mean(values(fc[Date.(timestamp(fc)).==t])))
-    end
-    wk_fc = TimeArray(MultiDay, fc_values)
-
-    add_forecast!(
-        c_sys5_hy_wk,
-        get_component(HydroGen, c_sys5_hy_wk, get_name(gen)),
-        Deterministic("get_max_active_power", wk_fc),
-    )
-end
-
-for gen in get_components(HydroEnergyReservoir, c_sys5_hy_uc)
-    for label in ["get_storage_capacity", "get_inflow"]
-        fc = get_forecast_values(
-            Deterministic,
-            gen,
-            get_forecast_initial_times(c_sys5_hy_uc)[1],
-            label,
-        )
-        fc_values = []
-        for (ix, t) in enumerate(MultiDay)
-            push!(fc_values, mean(values(fc[Date.(timestamp(fc)).==t])))
-        end
         wk_fc = TimeArray(MultiDay, fc_values)
-
-        add_forecast!(
-            c_sys5_hy_wk,
-            get_component(HydroGen, c_sys5_hy_wk, get_name(gen)),
-            Deterministic(label, wk_fc),
-        )
+        push!(ta,wk_fc)
     end
 end
+table = reduce(hcat, ta)
+TimeSeries.rename!(table, Symbol.(names(df)[names(df) .!= "DateTime"]))
+CSV.write(fname, table)
+=#
